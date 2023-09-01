3a5f58ec301e55a5842881eea009f39c2f932dae
Vinícius Silva <xdenik13@gmail.com>
2023-09-01 18:13:25 -0300

    Finalizando Nivelamento JPA

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -1,16 +1,31 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 	
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquin@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquin@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+		
+		EntityManagerFactory enf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager en = enf.createEntityManager();
+		
+		Pessoa p = en.find(Pessoa.class, 2);
+		en.getTransaction().begin();
+		en.remove(p);
+		en.getTransaction().commit();
+		
+		System.out.println(p);
+		
+		System.out.println("Pronto");
+		en.close();
+		enf.close();
 	}
 }
